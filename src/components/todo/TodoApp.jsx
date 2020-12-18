484f3a0b9e78d5a6aa87e308733cd7d08b009705
wa2l-99 <waelzantour1@gmail.com>
2020-12-18 01:55:06 +0100

    Chore: Create "LoginComponent" and Add state to it

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -4,11 +4,49 @@ class TodoApp extends Component{
     render(){
         return(
             <div className="TodoApp">
-                My todo App
+                <LoginComponent/>
             </div>
              
         );
     }
 }
+// the LoginComponent a controlled component.
+class LoginComponent extends Component{
+    constructor(props){
+        super(props)
+//We added state to this Logincomponent.
+        this.state ={
+            username: '',
+            password: ''
+        }
+    
+        this.handelChange=this.handelChange.bind(this);
+    }
+
+    // Added a generic event to handle all changes for any text element and we are now able to update the state whenever somebody types in something.
+
+    handelChange(event){
+       // this prints the state before the handleChange().
+        console.log(this.state);
+
+        this.setState({
+            [event.target.name]
+            : event.target.value
+        }
+        )
+
+    }
+
+
+    render(){
+      return(
+          <div>
+            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handelChange}/>
+            Password: <input type="password" name="password" value={this.state.password} onChange={this.handelChange}/>
+            <button>Login</button>
+        </div>
+      );
+    }
+  }
 
 export default TodoApp;
