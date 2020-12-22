a0b1d5119a0b6e783a2c599b306c9557b1fce9ad
SaharAchour <sahar.achour1999@gmail.com>
2020-12-22 18:18:46 +0100

    Chore: Add a comment

--- src/components/todo/AuthService.js
+++ src/components/todo/AuthService.js
@@ -1,3 +1,4 @@
+// Allow us to save the username in the session storage when we login and delete it when we logout
 class AuthService {
     registerSuccessfulLogin(username,password){
         sessionStorage.setItem('authenticatedUser', username);
