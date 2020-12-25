f322aaa124742d36b83092bdea7773bc19795087
wa2l-99 <waelzantour1@gmail.com>
2020-12-25 22:17:13 +0100

    Chore:Prepare the welcome page to connect with API

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,8 +1,15 @@
 import React , {Component} from 'react';
-import AuthService from './AuthService';
 import {Link} from "react-router-dom";
 class WelcomeComponent extends Component{
+    
+    constructor(props){
+        super(props)
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage(this);
+        
+    }
+
     render(){
+        
 
         return(
            <> 
@@ -11,8 +18,17 @@ class WelcomeComponent extends Component{
       
                 Welcome {this.props.match.params.name}. You Can Manage your todos <Link to='/todos'>here </Link>
             </div>
+            <div class="container">
+                Click here to get a customized welcome message.
+                <button onClick={this.retrieveWelcomeMessage} className='btn btn-success'>Get Welcome Message</button>
+            </div>
             </>
+            
+            
         )
+    }
+    retrieveWelcomeMessage(){
+        console.log('retrive clicked');
     }
 }
 export default WelcomeComponent; 
