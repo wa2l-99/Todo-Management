5a07523002f408c833d52fad603776412dc182d1
wa2l-99 <waelzantour1@gmail.com>
2020-12-29 02:05:04 +0100

    chore: Create the Delete Api Method

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -1,13 +1,18 @@
 import axios from 'axios'
 
-class TodoDataService{
+class TodoDataService {
 
 
-    
-    retrieveAllTodos(name){
+
+    retrieveAllTodos(name) {
         // console.log('executed service');
         return axios.get(`http://localhost:8080/users/${name}/todos`)
-         }
-    
+    }
+
+    deleteTode(name, id) {
+        return axios.delete(`http://localhost:8080/users/${name}/todos/${id}`)
+    }
+
+
 }
 export default new TodoDataService
